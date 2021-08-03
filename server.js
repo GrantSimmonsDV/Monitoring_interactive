fc8ac5dbf604c068e5e269b5e3ecc94094a801b1
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-08-03 12:47:42 -0600

    rollbar added

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express();
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '98eb2609dff54f3a93e06956944c58fb',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.use(express.json());
 
 //Student data
